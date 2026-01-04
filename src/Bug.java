56d61e44dca108d3ff2de06fa9cefa2f87509396
Urmila Yadav <urmila.yadav@ue-germany.de>
2026-01-04 02:34:24 +0100

    feat: add data validation to Bug setter

---
+++ src/Bug.java
@@ -0,0 +1,6 @@
+// A "Setter" - the professional way to update data
+    public void setTitle(String title) {
+        if (title != null && !title.isEmpty()) {
+            this.title = title;
+        }
+    }
